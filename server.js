ee6fae24053ea5ad1924c098f2d0a67c341d0e4f
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-02-13 13:29:28 -0500

    added UUID module for making ids, notes are displaying in browser

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const db = require('./db/db.json');
-const api = require('./routes/index')
+const api = require('./routes/index');
+const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('./helpers/fsUtils');
 
 const path = require('path');
@@ -19,12 +20,10 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-
 app.get('/api/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => {
         res.json(JSON.parse(data))
-        console.log('this is the data from the get request', data);
+        console.log('this is the data from the get request', JSON.parse(data));
     })
 })
 
@@ -40,13 +39,22 @@ app.post('/api/notes', (req, res) => {
         const newNote = {
             title,
             text,
+            id: uuidv4(),
         }
-
         readAndAppend(newNote, './db/db.json');
     } else {
         res.error('Error in adding tip!');
     }
 
 })
+
+app.delete('/api/notes/:id', (req, res) => {
+    res.json(`${req.method} request received`);
+    console.info(`${req.method} request received`);
+
+
+})
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
