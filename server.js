c0e9442146cc44243e78cdf188a5d9eb76e4d5e0
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-02-13 11:37:20 -0500

    created get route for notes.html path

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const db = require('./db/db.json');
+
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('*', (req, res) => {
+    res.send("This route doesn't exist");
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
